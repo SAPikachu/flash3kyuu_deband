9134b9246135004c75d4a51382bace1dce9d7b1d
SAPikachu <sapikachu@gmail.com>
2011-05-17 10:33:53 +0800

    extract common test code

--- correctness_test.cpp
+++ correctness_test.cpp
@@ -1,47 +1,7 @@
-
 #include "stdafx.h"
 
-#include "flash3kyuu_deband.h"
+#include "test.h"
 
-#include "impl_dispatch.h"
-
-#define DELEGATE_IMPL_CALL(impl, dest_buffer) impl(srcp, src_width, src_height, src_pitch, dest_buffer, dst_pitch, threshold, info_ptr_base, info_stride, range)
-
-#define GUARD_CONST 0xDEADBEEF
-
-unsigned char * create_guarded_buffer(int const height, int const pitch, unsigned char * &data_start)
-{
-	unsigned char* buffer = (unsigned char*)_aligned_malloc( (height + 4) * pitch, PLANE_ALIGNMENT);
-
-	for (int i = 0; i < pitch * 2; i += 4)
-	{
-		*(int*)(buffer + i) = GUARD_CONST;
-	}
-	for (int i = 0; i < pitch * 2; i += 4)
-	{
-		*(int*)(buffer + (height + 2) * pitch + i) = GUARD_CONST;
-	}
-	data_start = buffer + pitch * 2;
-	return buffer;
-}
-
-void check_guard_bytes(unsigned char *buffer, int const height, int const pitch)
-{
-	for (int i = 0; i < pitch * 2; i += 4)
-	{
-		if (*(int*)(buffer + i) != GUARD_CONST) {
-			printf("ERROR: Leading guard bytes was overwritten.\n");
-			break;
-		}
-	}
-	for (int i = 0; i < pitch * 2; i += 4)
-	{
-		if (*(int*)(buffer + (height + 2) * pitch + i) != GUARD_CONST) {
-			printf("ERROR: Trailing guard bytes was overwritten.\n");
-			break;
-		}
-	}
-}
 
 template<int, bool>
 void __cdecl process_plane_sse4_correctness_test(unsigned char const*srcp, int const src_width, int const src_height, int const src_pitch, unsigned char *dstp, int dst_pitch, unsigned char threshold, pixel_dither_info *info_ptr_base, int info_stride, int range)
---
+++ test.h
@@ -0,0 +1,42 @@
+
+#include "flash3kyuu_deband.h"
+
+#include "impl_dispatch.h"
+
+#define DELEGATE_IMPL_CALL(impl, dest_buffer) impl(srcp, src_width, src_height, src_pitch, dest_buffer, dst_pitch, threshold, info_ptr_base, info_stride, range)
+
+#define GUARD_CONST 0xDEADBEEF
+
+static unsigned char * create_guarded_buffer(int const height, int const pitch, unsigned char * &data_start)
+{
+	unsigned char* buffer = (unsigned char*)_aligned_malloc( (height + 4) * pitch, PLANE_ALIGNMENT);
+
+	for (int i = 0; i < pitch * 2; i += 4)
+	{
+		*(int*)(buffer + i) = GUARD_CONST;
+	}
+	for (int i = 0; i < pitch * 2; i += 4)
+	{
+		*(int*)(buffer + (height + 2) * pitch + i) = GUARD_CONST;
+	}
+	data_start = buffer + pitch * 2;
+	return buffer;
+}
+
+static void check_guard_bytes(unsigned char *buffer, int const height, int const pitch)
+{
+	for (int i = 0; i < pitch * 2; i += 4)
+	{
+		if (*(int*)(buffer + i) != GUARD_CONST) {
+			printf("ERROR: Leading guard bytes was overwritten.\n");
+			break;
+		}
+	}
+	for (int i = 0; i < pitch * 2; i += 4)
+	{
+		if (*(int*)(buffer + (height + 2) * pitch + i) != GUARD_CONST) {
+			printf("ERROR: Trailing guard bytes was overwritten.\n");
+			break;
+		}
+	}
+}
