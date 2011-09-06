b53d8d0b64a837df8c836cecdd0dea3552576523
SAPikachu <sapikachu@gmail.com>
2011-09-06 10:58:14 +0800

    force inline all emulation functions

--- sse_compat.h
+++ sse_compat.h
@@ -7,7 +7,7 @@
 
 #else
 
-static __m128i __inline _cmm_blendv_by_cmp_mask_epi8( 
+static __m128i __forceinline _cmm_blendv_by_cmp_mask_epi8( 
    __m128i a,
    __m128i b,
    __m128i mask 
@@ -20,7 +20,7 @@ static __m128i __inline _cmm_blendv_by_cmp_mask_epi8(
 	return _mm_or_si128(a_mask, b_mask);
 }
 
-static __m128i __inline _cmm_mullo_limit16_epi32(__m128i a, __m128i b)
+static __m128i __forceinline _cmm_mullo_limit16_epi32(__m128i a, __m128i b)
 {
 	// input can always be fit in 16bit
 
@@ -35,7 +35,7 @@ static __m128i __inline _cmm_mullo_limit16_epi32(__m128i a, __m128i b)
 	return _mm_unpacklo_epi16(lo_part, hi_part);
 }
 
-static __m128i __inline _mm_min_epu16(__m128i val1, __m128i val2)
+static __m128i __forceinline _mm_min_epu16(__m128i val1, __m128i val2)
 {
     __m128i sign_convert = _mm_set1_epi16((short)0x8000);
     val1 = _mm_sub_epi16(val1, sign_convert);
@@ -49,19 +49,19 @@ static __m128i __inline _mm_min_epu16(__m128i val1, __m128i val2)
 
 #if !defined(SSE_LIMIT) || SSE_LIMIT >= 31
 
-static __m128i __inline _cmm_negate_all_epi32(__m128i a, __m128i minus_one)
+static __m128i __forceinline _cmm_negate_all_epi32(__m128i a, __m128i minus_one)
 {
 	return _mm_sign_epi32(a, minus_one);
 }
 
 #else
 
-static __m128i __inline _cmm_negate_all_epi32(__m128i a, __m128i minus_one)
+static __m128i __forceinline _cmm_negate_all_epi32(__m128i a, __m128i minus_one)
 {
 	return _mm_sub_epi32(_mm_setzero_si128(), a);
 }
 
-static __m128i __inline _mm_abs_epi32 (__m128i a)
+static __m128i __forceinline _mm_abs_epi32 (__m128i a)
 {
     __m128i mask = _mm_srai_epi32(a, 31);
     __m128i fix = _mm_srli_epi32(a, 31);
