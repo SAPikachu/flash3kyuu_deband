c93d9ef0b01b049b0f35dfb315b7475c4e7d4a57
SAPikachu <sapikachu@gmail.com>
2013-08-18 18:31:03 +0800

    __cdecl is not needed here

--- core.h
+++ core.h
@@ -58,7 +58,7 @@ typedef struct _process_plane_params
     }
 } process_plane_params;
 
-typedef void (__cdecl *process_plane_impl_t)(const process_plane_params& params, process_plane_context* context);
+typedef void (*process_plane_impl_t)(const process_plane_params& params, process_plane_context* context);
 
 class f3kdb_core_t {
 private:
