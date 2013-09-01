84f9349bb0541ddfab78a352dba9256e2b2fc5ca
SAPikachu <sapikachu@gmail.com>
2013-09-01 17:12:07 +0800

    Remove f3kdb_dither

--- src/avisynth/avisynth_init.cpp
+++ src/avisynth/avisynth_init.cpp
@@ -1,6 +1,5 @@
 #include "stdafx.h"
 #include "filter.h"
-#include "dither_avs.h"
 #include "../compiler_compat.h"
 
 F3KDB_API(const char*) AvisynthPluginInit2(IScriptEnvironment* env)
@@ -12,10 +11,6 @@ F3KDB_API(const char*) AvisynthPluginInit2(IScriptEnvironment* env)
 	env->AddFunction("f3kdb", 
 		F3KDB_AVS_PARAMS, 
 		Create_flash3kyuu_deband, 
-		NULL);
-	env->AddFunction("f3kdb_dither", 
-		"c[mode]i[stacked]b[input_depth]i[keep_tv_range]b", 
-		Create_dither, 
 		NULL);
 
 	return "f3kdb";
