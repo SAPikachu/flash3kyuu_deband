ffe2a9b99652784a23443e4f848f63a7cf6847b0
SAPikachu <sapikachu@gmail.com>
2013-09-07 09:58:24 +0800

    Move preset definition to separate file, and some new presets

--- src/public_interface.cpp
+++ src/public_interface.cpp
@@ -11,24 +11,9 @@
 #include "auto_utils.h"
 #include "constants.h"
 #include "impl_dispatch.h"
+#include "presets.h"
 
 using namespace std;
-
-static const char* const PRESETS[] = {
-    "depth",
-    "y=0/cb=0/cr=0/grainy=0/grainc=0",
-    "low",
-    "y=32/cb=32/cr=32/grainy=32/grainc=32",
-    "medium",
-    "y=48/cb=48/cr=48/grainy=48/grainc=48",
-    "high",
-    "y=64/cb=64/cr=64/grainy=64/grainc=64",
-    "veryhigh",
-    "y=80/cb=80/cr=80/grainy=80/grainc=80",
-    "nograin",
-    "grainy=0,grainc=0",
-    nullptr, nullptr
-};
 
 F3KDB_API(int) f3kdb_params_init_defaults(f3kdb_params_t* params, int interface_version)
 {
