b2987d119b12debfdffc3cbde4e8c23e9b484bf1
Xinyue Lu <i@7086.in>
2019-06-26 03:53:15 -0400

    Remove high depth mode and depth

--- src/auto_utils.cpp
+++ src/auto_utils.cpp
@@ -25,7 +25,6 @@ void params_set_defaults(f3kdb_params_t* params)
     params->opt = IMPL_AUTO_DETECT;
     params->dither_algo = DA_HIGH_FLOYD_STEINBERG_DITHERING;
     params->keep_tv_range = false;
-    params->output_mode = DEFAULT_PIXEL_MODE;
     params->output_depth = -1;
     params->random_algo_ref = RANDOM_ALGORITHM_UNIFORM;
     params->random_algo_grain = RANDOM_ALGORITHM_UNIFORM;
@@ -48,7 +47,6 @@ int params_set_by_string(f3kdb_params_t* params, const char* name, const char* v
     if (!_stricmp(name, "opt")) { return params_set_value_by_string(&params->opt, value_string); }
     if (!_stricmp(name, "dither_algo")) { return params_set_value_by_string(&params->dither_algo, value_string); }
     if (!_stricmp(name, "keep_tv_range")) { return params_set_value_by_string(&params->keep_tv_range, value_string); }
-    if (!_stricmp(name, "output_mode")) { return params_set_value_by_string(&params->output_mode, value_string); }
     if (!_stricmp(name, "output_depth")) { return params_set_value_by_string(&params->output_depth, value_string); }
     if (!_stricmp(name, "random_algo_ref")) { return params_set_value_by_string(&params->random_algo_ref, value_string); }
     if (!_stricmp(name, "random_algo_grain")) { return params_set_value_by_string(&params->random_algo_grain, value_string); }
